9409f838d03ad95c26035a1ac93b1e96bf71ab7a
bennedictbett <benedictbett08@gmail.com>
2026-06-05 07:54:27 +0300

    fix: add Vercel domain to CORS

--- app/main.py
+++ app/main.py
@@ -52,8 +52,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:8501",
-        "http://127.0.0.1:8501",
+        "https://*.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
